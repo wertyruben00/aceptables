937f302efdb8cb8a53168aa81a47032c792ae63a
GabrielMarBarrio <49036708+GabrielMarBarrio@users.noreply.github.com>
2019-04-06 09:59:12 -0700

    Add files via upload

---
+++ Maximo camino.cpp
@@ -0,0 +1,64 @@
+#include<stdio.h>
+int maxim(int a , int b){
+    if(a>b) return a;
+
+    return b;
+}
+int main(){
+    int m, n;
+    scanf("%d %d",&m,&n);
+    int arr[][n];
+    for(int i = 0 ; i < m ; i ++)
+        for(int j = 0 ; j < n ; j ++)
+            scanf("%d",&arr[i][j]);
+
+    int i = m , j = n ;
+     for(int k = n - 1 ; k > 0; k --){
+        i = m -1;
+        j = k;
+        while(j <= n - 1 ){
+            if(i == m - 1 && j == n - 1){
+                i--;
+                j++;
+            }else if(i == m - 1 && j < n - 1){
+                arr[i][j] = arr[i][j]+arr[i][j + 1];
+                i--;
+                j++;
+            }else if(j == n - 1 && i < m - 1){
+                arr[i][j] = arr[i][j]+arr[i + 1][j];
+                i--;
+                j++;
+            }else{
+                arr[i][j] = arr[i][j] + maxim(arr[i + 1][j],arr[i][j + 1]);
+                i--;
+                j++;
+            }
+        }
+    }
+    for(int k = m - 1 ; k >= 0  ; k--){
+        i = k;
+        j = 0;
+        while(i >= 0 && j < n){
+            if(i == m - 1){
+                arr[i][j] = arr[i][j]+arr[i][j + 1];
+                i--;
+                j++;
+            }else if(j == n - 1){
+                arr[i][j] = arr[i][j]+arr[i + 1][j];
+                i--;
+                j++;
+            }else{
+                arr[i][j] = arr[i][j] + maxim(arr[i + 1][j],arr[i][j + 1]);
+                i--;
+                j++;
+            }
+        }
+    }
+    for(int i = 0 ; i < m ; i++){
+        for(int  j = 0 ; j < n ; j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
