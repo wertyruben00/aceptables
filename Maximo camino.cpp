dee35b44e14695201b1106b0fba86e4a90779f60
GabrielMarBarrio <49036708+GabrielMarBarrio@users.noreply.github.com>
2019-04-06 10:00:02 -0700

    Update Maximo camino.cpp

--- Maximo camino.cpp
+++ Maximo camino.cpp
@@ -7,7 +7,7 @@ int maxim(int a , int b){
 int main(){
     int m, n;
     scanf("%d %d",&m,&n);
-    int arr[][n];
+    int arr[m][n];
     for(int i = 0 ; i < m ; i ++)
         for(int j = 0 ; j < n ; j ++)
             scanf("%d",&arr[i][j]);
